f6f175674a4b29209c07b2fa3a68448067d9aec0
Piotr Yartsev <piotr_1997@live.se>
2023-02-03 13:00:34 +0100

    Create Sod-shick-tube.py

---
+++ Project2/Sod-shick-tube.py
@@ -0,0 +1,12 @@
+
+
+initial_conditions_x_less_or_equal_0=[1,0,2.5,1,0.001875,0.001875,320]
+initial_conditions_x_greater_0=[0.25,0,1.795,0.1795,0.0075,0.001875,80]
+
+
+
+"""
+a_d=1/h
+W_R_largen_or_equal_2=0
+W_R_between_1_and_2= lambda R: a_d*1/6(2-R)**3
+W_R_between_0_and_1= lambda R: a_d*2/3-R**2+1/2*R**3"""
